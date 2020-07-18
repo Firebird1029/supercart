cf7877173fd0504a844765b0a13ba560040138ad
Brandon <6111102+Firebird1029@users.noreply.github.com>
2020-07-17 23:18:54 -1000

    Working Demo Order

--- tests/test1.py
+++ tests/test1.py
@@ -26,14 +26,31 @@ vc = ViewClient(device, serialno)
 
 print("Setup complete")
 
-device.dragDip((150.0, 200.0), (150.0, 400.0), 40, 20, 0) # scroll down to refresh
+# Jodi's Demo Order
+# vc.findViewWithTextOrRaise(u'Safeway Demo - 5 items', root=vc.findViewByIdOrRaise('id/no_id/6')).touch()
+# time.sleep(5)
+# device.drag((100, 1350), (660, 1350), 45, 20, 0)
+
+# Scroll down to refresh
+# device.drag((350, 350), (350, 950), 40, 20, 0)
+# device.dragDip((150.0, 200.0), (150.0, 400.0), 40, 20, 0)
 # vc.findViewByIdOrRaise(u"com.instacart.shopper:id/fragment_dashboard_native_card_recyclerview").uiScrollable.flingBackward()
 
-print(vc.findViewByIdOrRaise("com.instacart.shopper:id/is_dashboardVirtualBatchStatus_title").getText())
+# Touch
+# vc.findViewWithTextOrRaise(u'Order now').touch()
+
+# Swipe
+# device.drag((100, 1350), (660, 1350), 45, 20, 0)
+
+# Debug
+# print(vc.findViewByIdOrRaise("com.instacart.shopper:id/is_dashboardVirtualBatchStatus_title").getText())
 # print(vc.findViewByIdOrRaise("com.instacart.shopper:id/fragment_dashboard_native_card_recyclerview").getChildren())
 # for child in vc.findViewByIdOrRaise("com.instacart.shopper:id/fragment_dashboard_native_card_recyclerview").getChildren():
 #     print(child)
 
-for i in range(0, 5):
-	device.dragDip((150.0, 200.0), (150.0, 400.0), 40, 20, 0)
-	time.sleep(1)
+# Repeat
+# for i in range(0, 5):
+# 	device.dragDip((150.0, 200.0), (150.0, 400.0), 40, 20, 0)
+# 	time.sleep(1)
+
+print("Done")
